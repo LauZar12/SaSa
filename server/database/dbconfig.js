d81b9dae3cb9484c630d0238949c0ccf90240b4b
vaalmo <valentinamoralesvillada17@outlook.com>
2024-09-07 17:20:22 -0500

    (update): organized backend structure, and added dynamoose config

---
+++ server/database/dbconfig.js
@@ -0,0 +1,7 @@
+import dynamoose from "dynamoose"
+
+dynamoose.aws.sdk.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_DEFAULT_REGION,
+})
--- server/index.js
+++ server/index.js
@@ -1,17 +1,16 @@
-import express from 'express';
+import app from './app.js'
+
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv'
 dotenv.config()
 
-const app = express();
-
 app.use(bodyParser.json({ limit: "30mb", extended:true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended:true }));
 app.use(cors());
 
-
-app.listen(process.env.PORT, () => console.log(`Server running on port: ${process.env.PORT}`))
+app.listen(process.env.PORT)
+console.log(`Server running on port: ${process.env.PORT}`)
 
 
 
