ad0842d3729daedd31774364c409b3d9bed2c9fe
vaalmo <valentinamoralesvillada17@outlook.com>
2024-09-10 21:25:33 -0500

    Merge branch 'main' of https://github.com/LauZar12/SaSa

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,24 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   optimizeDeps: {
-    include: ['@mui/icons-material'],
+    include: [
+      '@emotion/react', 
+      '@emotion/styled', 
+      '@mui/material/Tooltip'
+    ],
   },
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
 })
