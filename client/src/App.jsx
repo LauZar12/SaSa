a88a675955959c750aaa9767ef2c5632c923a79c
Sebasalazaro <sebasalazaro@gmail.com>
2024-10-20 16:40:57 -0500

    Integración mapa sencillo con BottomBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
 import { Route, Routes } from "react-router-dom";
+
 import Auth from "./sections/Auth";
 import Home from "./sections/Home";
 import Businesses from "./sections/Businesses";
-import AdminDashboard from "./sections/AdminDashboard"; // Importa tu componente del Admin Dashboard
+import AdminDashboard from "./sections/AdminDashboard";
 import Business from "./sections/Business";
 import EditProduct from "./sections/EditProduct";
+import MapV2 from "./sections/MapV2";
 import toast, { Toaster } from 'react-hot-toast';
+import {APIProvider, Map} from '@vis.gl/react-google-maps';
 
 export default function App() {
+  
   return (
     <>
+
       <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -17,7 +22,7 @@ export default function App() {
         <Route path="/businesses" element={<Businesses />} />
         <Route
           path="/businesses/:businessId"
-          element={<Business />} // No pasar el businessId como prop
+          element={<Business />} 
         />
         <Route 
           path="/admin/businesses/:businessId" 
@@ -26,6 +31,23 @@ export default function App() {
         <Route 
           path="/admin/businesses/:businessId/products/:productId/edit-product" 
           element={<EditProduct />} 
+        />
+        <Route 
+          path="/map" 
+          element={
+            <>
+              <APIProvider apiKey={'AIzaSyBbOxklM1Vcm_wT6wzSnhKJa4LvR1jvYnk'}>
+                <Map
+                style={{width: '100vw', height: '100vh'}}
+                defaultCenter={{lat: 22.54992, lng: 0}}
+                defaultZoom={3}
+                gestureHandling={'greedy'}
+                disableDefaultUI={true}
+                />
+              </APIProvider>
+              <MapV2 />
+            </>  
+          } 
         />
       </Routes>
     </>
---
+++ client/src/sections/MapV2.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import BottomNavBar from '../components/BottomNavBar'
+
+const MapV2 = () => {
+  return (
+    <BottomNavBar />
+  )
+}
+
+export default MapV2
