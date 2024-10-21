4630a825693d72cca2267b093f365854cc888da0
Sebasalazaro <sebasalazaro@gmail.com>
2024-10-21 13:16:51 -0500

    (feature): Added Map with Markers

--- client/src/sections/MapV2.jsx
+++ client/src/sections/MapV2.jsx
@@ -1,87 +1,58 @@
-// import React from 'react'
-// import BottomNavBar from '../components/BottomNavBar'
+import React from 'react';
+import {APIProvider, Map, Pin, AdvancedMarker} from '@vis.gl/react-google-maps';
 
-// const MapV2 = () => {
-//   return (
-//     <BottomNavBar />
-    
-//   )
-// }
+const locations = [
+    {key: 'Frisby Eafit', location: { lat: 6.199387481828809, lng: -75.57807649683409  }},
+    {key: 'McDonalds Laureles', location: { lat: 6.244928475806693, lng: -75.59537447538722 }},
+    {key: 'SportsTown VivaEnvigado', location: { lat: 6.1781810867482605, lng: -75.59119123892503 }},
 
-// export default MapV2
+];
+  
 
-// 
-
-
-/* global google */
-import {
-  GoogleMap,
-  InfoWindow,
-  Marker,
-  useLoadScript,
-} from "@react-google-maps/api";
-import { useState } from "react";
-import "../Map.css";
-
-const MapV2 = () => {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyBbOxklM1Vcm_wT6wzSnhKJa4LvR1jvYnk',
-  });
-  const [mapRef, setMapRef] = useState();
-  const [isOpen, setIsOpen] = useState(false);
-  const [infoWindowData, setInfoWindowData] = useState();
-  const markers = [
-    { address: "Las vegas con 7 sur, El Poblado, Medellín, Antioquia", lat: 6.199387481828809, lng: -75.57807649683409 },
-    { address: "Calle 39B, Cq. 73B #N° - 67, Medellín, Antioquia", lat: 6.244928475806693, lng: -75.59537447538722 },
-    { address: "Zona 1, Envigado, Antioquia", lat: 6.1781810867482605, lng: -75.59119123892503 }
-  ];
-
-  const onMapLoad = (map) => {
-    setMapRef(map);
-    const bounds = new google.maps.LatLngBounds();
-    markers?.forEach(({ lat, lng }) => bounds.extend({ lat, lng }));
-    map.fitBounds(bounds);
-  };
-
-  const handleMarkerClick = (id, lat, lng, address) => {
-    mapRef?.panTo({ lat, lng });
-    setInfoWindowData({ id, address });
-    setIsOpen(true);
-  };
-
+const PoiMarkers = (props) => {
   return (
-    <div className="App">
-      {!isLoaded ? (
-        <h1>Loading...</h1>
-      ) : (
-        <GoogleMap
-          mapContainerClassName="map-container"
-          onLoad={onMapLoad}
-          onClick={() => setIsOpen(false)}
+    <>
+      {props.pois.map((poi) => (
+        <AdvancedMarker
+          key={poi.key}
+          position={poi.location}
+          // clickable={true}
+          // onClick={handleClick}
         >
-          {markers.map(({ address, lat, lng }, ind) => (
-            <Marker
-              key={ind}
-              position={{ lat, lng }}
-              onClick={() => {
-                handleMarkerClick(ind, lat, lng, address);
-              }}
-            >
-              {isOpen && infoWindowData?.id === ind && (
-                <InfoWindow
-                  onCloseClick={() => {
-                    setIsOpen(false);
-                  }}
-                >
-                  <h3>{infoWindowData.address}</h3>
-                </InfoWindow>
-              )}
-            </Marker>
-          ))}
-        </GoogleMap>
-      )}
-    </div>
+          <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
+        </AdvancedMarker>
+      ))}
+    </>
   );
 };
 
+
+// const handleClick = useCallback((ev) => {
+//   if (!map) return;
+//   if (!ev.latLng) return;
+//   console.log('marker clicked:', ev.latLng.toString());
+//   map.panTo(ev.latLng);
+// }, [map]);
+
+
+
+const MapV2 = () => (
+ <APIProvider apiKey={'AIzaSyBbOxklM1Vcm_wT6wzSnhKJa4LvR1jvYnk'} onLoad={() => console.log('Maps API has loaded.')}>
+    <Map
+      defaultZoom={13}
+      defaultCenter={ { lat: 6.249316307398763, lng: -75.5606163100253 } }
+      mapId='274f8ffa0113d548'
+      style={{width: '100vw', height: '100vh'}}
+      gestureHandling={'greedy'}
+      disableDefaultUI={true}
+    >
+    <PoiMarkers pois={locations} />
+    </Map>
+ </APIProvider>
+);
+
 export default MapV2;
+
+//     { address: "Las vegas con 7 sur, El Poblado, Medellín, Antioquia", lat: 6.199387481828809, lng: -75.57807649683409 },
+//     { address: "Calle 39B, Cq. 73B #N° - 67, Medellín, Antioquia", lat: 6.244928475806693, lng: -75.59537447538722 },
+//     { address: "Zona 1, Envigado, Antioquia", lat: 6.1781810867482605, lng: -75.59119123892503 }
