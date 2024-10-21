1e4755a373f06ded0c67bf6d20d40f81998c830b
vaalmo <valentinamoralesvillada17@outlook.com>
2024-10-21 11:55:54 -0500

    (feature): edit surprise box and create modified

--- server/routes/surprisebox.routes.js
+++ server/routes/surprisebox.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-import { createSurpriseBox, getAllSurpriseBoxesFromBusiness } from '../controllers/surprisebox.controllers.js';
+import { createSurpriseBox, editSurpriseBox, getAllSurpriseBoxesFromBusiness } from '../controllers/surprisebox.controllers.js';
 
 const router = Router();
 
 router.post('/admin/businesses/:businessId/surprise-boxes/create-surprise-box', createSurpriseBox)
 router.get('/admin/businesses/:businessId/surprise-boxes', getAllSurpriseBoxesFromBusiness)
+router.put('/admin/businesses/:businessId/surprise-boxes/:surpboxId/edit-surprise-box', editSurpriseBox)
 
 export default router;
